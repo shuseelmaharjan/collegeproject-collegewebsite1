04f810518914b1d58280abadd34c433c339ff45f
Shuseel Maharjan <maharzanshuseel10@gmail.com>
2022-08-12 07:51:18 +0545

    first commit

---
+++ assets/main.js
@@ -0,0 +1,15 @@
+var slide = document.getElementById("slider");
+var btn1=document.getElementById("btn1");
+var btn2=document.getElementById("btn2");
+var btn3=document.getElementById("btn3");
+
+
+btn1.onclick=function(){
+    slide.style.transform="translateX(0px)";
+}
+btn2.onclick=function(){
+    slide.style.transform="translateX(-100%)";
+}
+btn3.onclick=function(){
+    slide.style.transform="translateX(-200%)";
+}
